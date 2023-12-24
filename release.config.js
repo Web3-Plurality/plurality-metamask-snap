bebbb03e541888a7276837b1b73ca45c376839c9
Mujtaba Idrees <mujtabaidrees@gmail.com>
2023-12-24 10:36:01 -0800

    fix: Remove pushing package json semantic release update

--- release.config.js
+++ release.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/npm',
-    '@semantic-release/git',
-  ],
-
-  git: {
-    assets: ['package.json', 'README.md'],
-    message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-  }
+  branches: ['main']
 };
