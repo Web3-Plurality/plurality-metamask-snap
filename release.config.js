1b9d2142318275b3b50fb4605d22605d0c8287c4
Mujtaba Idrees <mujtabaidrees@gmail.com>
2023-12-24 10:00:40 -0800

    fix: Update package json file with semantic release version

--- release.config.js
+++ release.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  branches: ['main']
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+
+  git: {
+    assets: ['package.json', 'README.md'],
+    message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+  }
 };
