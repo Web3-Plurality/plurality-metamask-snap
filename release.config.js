8b82afecddba5ecc9917e3a197d95c1de0546b69
Mujtaba Idrees <mujtabaidrees@gmail.com>
2023-12-24 21:39:32 -0800

    fix: Update version in code via pipeline

--- release.config.js
+++ release.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  branches: ['main']
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+
+  git: {
+    assets: ['package.json', 'snap.manifest.json'],
+    message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+  }
 };
+
